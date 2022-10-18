dedaa04b890debd0c84c11c58987e0930994d1ba
MundiaNderi <sarahngima77@gmail.com>
2022-10-18 07:35:28 +0300

    Lots of alphabets soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase
+ * followed by a new line. except q and e
+ * Return: 0
+*/
+int main(void)
+{
+char la;
+for (la = 'a'; la <= 'z' la++)
+{
+if (la != 'e' && la != 'q')
+putchar(la);
+
+return (0);
+}
