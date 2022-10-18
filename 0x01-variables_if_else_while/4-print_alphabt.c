ac623df7a4f3e811dc8729bc6daa7c25e0ea3b51
MundiaNderi <sarahngima77@gmail.com>
2022-10-18 09:54:57 +0300

    fixing soups

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,13 @@ int main(void)
 char la;
 
 for (la = 'a'; la <= 'z'; la++)
-	if (la != 'e' && la != 'q')
-		putchar(la);
-
-	putchar('\n');
+{
+if (la != 'e' && la != 'q')
+{
+putchar(la);
+}
+}
+putchar('\n');
 
 return (0);
 }
