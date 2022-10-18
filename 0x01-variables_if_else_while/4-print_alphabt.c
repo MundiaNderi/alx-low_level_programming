cb93f7004377e634b186b500fdd5812663417fb8
MundiaNderi <sarahngima77@gmail.com>
2022-10-18 09:44:02 +0300

    Alphabet soups

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 {
 char la;
 
-for (la = 'a'; la <= 'z; la++)
-if (la != 'e' && la != 'q')
-putchar(la);
+for (la = 'a'; la <= 'z'; la++)
+	if (la != 'e' && la != 'q')
+		putchar(la);
 
-putchar('\n');
+	putchar('\n');
 
 return (0);
 }
