6c473e6812c30a3957c0537a87f42532b93ed360
MundiaNderi <sarahngima77@gmail.com>
2022-10-18 11:41:35 +0300

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible combinations of sigle-digit num
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{if n != 57)
+	{
+		putchar(',')'
+		putchar(' ');
+	}
+	putchar('\n');
+	return(0);
+}
