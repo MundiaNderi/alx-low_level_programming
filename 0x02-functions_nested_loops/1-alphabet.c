7851dfc393b344b1641c56de659f5455f8ec06a5
MundiaNderi <sarahngima77@gmail.com>
2022-10-19 10:23:53 +0300

    Return void

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet - print all alphabet in lowercase
- * Return: 0
  */
 void print_alphabet(void)
 {
@@ -11,5 +10,4 @@ void print_alphabet(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
-	return (0);
 }
