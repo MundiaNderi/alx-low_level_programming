cb7c2aaf63c3cd64481537e0969a4513ac2ea87e
MundiaNderi <sarahngima77@gmail.com>
2022-10-28 12:03:43 +0300

    Fixing task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -27,7 +27,7 @@ char *rot13(char *str)
 					'r', 's', 't', 'u', 'v', 'w',
 					'x', 'y', 'z', 'a', 'b', 'c',
 					'd', 'e', 'f', 'g', 'h', 'j',
-					'k', 'l', 'm',};
+					'k', 'l', 'm'};
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
