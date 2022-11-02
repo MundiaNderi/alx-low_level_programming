3f85288268a0e180da957bd06cafbe22158a6b12
MundiaNderi <sarahngima77@gmail.com>
2022-11-02 21:34:34 +0300

    fixing task 4 comments

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,7 +3,7 @@
 
 /**
  * _strpbrk - searches a string for any set of bytes.
- * @s: locate in this string the first occurence of the bytes in the string @accept
+ * @s: locate the first occurence of the bytes in the string @accept
  * @accept: contains bytes to be comapred to bytes in string @s.
  * Return: a pointer to the byte in s that matches one of the bytes in accept,
  * Return NULL if no such byte is found.
