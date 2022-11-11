85e9f015112c253ae63b59d26fa5ec6e84407b4d
MundiaNderi <sarahngima77@gmail.com>
2022-11-12 01:37:28 +0300

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* malloc_checked - allocate a space.
+* @b: unsigned int.
+* Return: void
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+	if (x == NULL)
+	{
+		exit(98);
+	}
+	return (x);
+}
