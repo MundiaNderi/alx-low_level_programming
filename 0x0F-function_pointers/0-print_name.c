120a69363648662ee9e763a401529a8dced160ee
MundiaNderi <sarahngima77@gmail.com>
2022-11-18 22:38:50 +0300

    Task one fixes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 * print_name - prints a name
 * @name: the name to print
 * @f: the function to send the name to
-* Return: Always 0 (Success)
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+	if (!name || !f)
+		return;
+	f(name);
 }
