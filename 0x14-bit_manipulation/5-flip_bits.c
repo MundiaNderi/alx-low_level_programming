592f59d85c52c0fed84e6514ff6e9e2765972ef7
MundiaNderi <sarahngima77@gmail.com>
2022-12-06 10:34:28 +0300

    Fixing task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,21 +3,22 @@
 /**
 * flip_bits -  returns the number of bits you would need to flip
 * to get from one number to another.
-* @n: parameter.
-* @m: destiny.
-* Return: nflips
+* @n: the first number.
+* @m: the second number
+* Return: the number of flipped bits.
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, nflips = 0;
-	unsigned long int j = sizeof(unsigned long int) * 8;
+	unsigned int xor = n ^ m;
+	int count = 0;
 
-	for (i = 0; i < j; j++)
+	if (!n || !m)
+		return (0);
+	while (xor)
 	{
-		if ((m & 1) != (n & 1))
-			nflips += 1;
-		n = n >> 1;
-		m = m >> 1;
+		if (xor & 1)
+			count++;
+		xor = xor >> 1;
 	}
-	return (nflips);
+	return (count);
 }
