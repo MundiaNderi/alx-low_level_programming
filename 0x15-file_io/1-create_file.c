1be9b9b2ca6b3a67d45c04f3641d7bc180823b12
MundiaNderi <sarahngima77@gmail.com>
2022-12-08 21:08:35 +0300

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,29 +3,36 @@
 /**
 * create_file - creates a file.
 * @filename: The name of the file to be created.
-* @text_content - content to be placed in the created file.
+* @text_content: content to be placed in the created file.
 * Return: 1 if successful, and -1 on err.
 */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fild = 0, chk = 0, i = 0;
+	int len;
+	int fd, wr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	fild = open(filename, O_WRONLY | O_CREATE | O_TRUNC, 00600);
-	if 9fild < 0)
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
+	return (1);
 
-	while (text_content[i])
-		i++;
-	chk = write(fild, text_content, i)
-	if (chk < 0)
+	for (len = 0; text_content[len]; len++)
+	{}
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+	{
+		close(fd);
 		return (-1);
-
-	close(fild);
+	}
+	close(fd);
 	return (1);
 }
