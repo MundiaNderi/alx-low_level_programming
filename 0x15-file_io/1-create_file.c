bc7aee797a80e86ffe8a4c9ca69fed604ea03673
MundiaNderi <sarahngima77@gmail.com>
2022-12-11 21:28:00 +0300

    Fixing task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,31 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fild = 0, chk = 0, i = 0;
+	int len;
+	int fd, wr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
-	if (fild < 0)
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	while (text_content[i]
-		i++;
-	chk = write(fild, texy_content, i)
-	if (chk < 0)
+	for (len = 0; text_content[len]; len++)
+	{}
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+	{
+		close(fd);
 		return (-1);
-
-	close(fild);
+	}
+	close(fd);
 	return (1);
 }
